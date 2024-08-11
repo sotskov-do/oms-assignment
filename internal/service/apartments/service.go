6fbfaa091e4770f53df6d1122008c4766d8c40ca
Denis Sotskov <sotskov.do@gmail.com>
2024-08-11 18:28:49 +0300

    added building and apartment services

---
+++ internal/service/apartments/service.go
@@ -0,0 +1,89 @@
+package apartments
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/sotskov-do/oms-assignment/internal/models"
+	"github.com/sotskov-do/oms-assignment/internal/storage"
+)
+
+type ApartmentsService interface {
+	GetApartments(ctx context.Context) (models.ApartmentSlice, error)
+	GetApartment(ctx context.Context, id int) (*models.Apartment, error)
+	GetApartmentsInBuilding(ctx context.Context, buildingId int) (models.ApartmentSlice, error)
+	CreateApartment(ctx context.Context, apartment *models.Apartment) error
+	DeleteApartment(ctx context.Context, id int) error
+}
+
+type Service struct {
+	apartmentsStorage storage.ApartmentsStorage
+}
+
+func NewService(apartmentsStorage storage.ApartmentsStorage) *Service {
+	return &Service{
+		apartmentsStorage: apartmentsStorage,
+	}
+}
+
+func (s *Service) GetApartments(ctx context.Context) (models.ApartmentSlice, error) {
+	apartments, err := s.apartmentsStorage.GetApartments(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return apartments, nil
+}
+
+func (s *Service) GetApartment(ctx context.Context, id int) (*models.Apartment, error) {
+	if id <= 0 {
+		return nil, errors.New("id less or equal 0")
+	}
+
+	apartment, err := s.apartmentsStorage.GetApartment(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return apartment, nil
+}
+
+func (s *Service) GetApartmentsInBuilding(ctx context.Context, buildingId int) (models.ApartmentSlice, error) {
+	if buildingId <= 0 {
+		return nil, errors.New("building id less or equal 0")
+	}
+
+	apartmentsInBuilding, err := s.apartmentsStorage.GetApartmentsInBuilding(ctx, buildingId)
+	if err != nil {
+		return nil, err
+	}
+
+	return apartmentsInBuilding, nil
+}
+
+func (s *Service) CreateApartment(ctx context.Context, apartment *models.Apartment) error {
+	err := s.apartmentsStorage.CreateApartment(ctx, apartment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) DeleteApartment(ctx context.Context, id int) error {
+	if id <= 0 {
+		return errors.New("id less or equal 0")
+	}
+
+	n, err := s.apartmentsStorage.DeleteApartment(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	if n == 0 {
+		return fmt.Errorf("no apartment with id [%v]", id)
+	}
+
+	return nil
+}
---
+++ internal/service/buildings/service.go
@@ -0,0 +1,74 @@
+package buildings
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sotskov-do/oms-assignment/internal/models"
+	"github.com/sotskov-do/oms-assignment/internal/storage"
+)
+
+type BuildingsService interface {
+	GetBuildings(ctx context.Context) (models.BuildingSlice, error)
+	GetBuilding(ctx context.Context, id int) (*models.Building, error)
+	CreateBuilding(ctx context.Context, building *models.Building) error
+	DeleteBuilding(ctx context.Context, id int) error
+}
+
+type Service struct {
+	buildingsStorage storage.BuildingsStorage
+}
+
+func NewService(buildingsStorage storage.BuildingsStorage) *Service {
+	return &Service{
+		buildingsStorage: buildingsStorage,
+	}
+}
+
+func (s *Service) GetBuildings(ctx context.Context) (models.BuildingSlice, error) {
+	buildings, err := s.buildingsStorage.GetBuildings(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return buildings, nil
+}
+
+func (s *Service) GetBuilding(ctx context.Context, id int) (*models.Building, error) {
+	if id <= 0 {
+		return nil, errors.New("id less or equal 0")
+	}
+
+	building, err := s.buildingsStorage.GetBuilding(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return building, nil
+}
+
+func (s *Service) CreateBuilding(ctx context.Context, building *models.Building) error {
+	err := s.buildingsStorage.CreateBuilding(ctx, building)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) DeleteBuilding(ctx context.Context, id int) error {
+	if id <= 0 {
+		return errors.New("id less or equal 0")
+	}
+
+	n, err := s.buildingsStorage.DeleteBuilding(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	if n == 0 {
+		return errors.New("no building with such id")
+	}
+
+	return nil
+}
